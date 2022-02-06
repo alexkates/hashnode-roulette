adef4bc5e9f92d532d89fe15b3b8fb18843f729b
Daniel Alexander Kates <alex.kates@creditly.com>
2022-02-05 20:35:01 -0500

    refactored a Button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from "next"
 import { Session } from "next-auth"
 import { signOut, signIn } from "next-auth/react"
 import { getSession } from "next-auth/react"
+import { Button } from "../components/Button"
 import { Card } from "../components/Card"
 
 type IndexPageProps = {
@@ -16,27 +17,17 @@ export default function IndexPage({ session }: IndexPageProps) {
           <h5 className="text-gray-900 text-2xl font-medium mb-4">
             Hashnode Roulette
           </h5>
-
-          <button
-            type="button"
-            onClick={() => signOut()}
-            className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          >
-            Sign out
-          </button>
+          <Button onClick={() => signOut()} text="Sign out" />
         </Card>
       ) : (
         <Card>
           <h5 className="text-gray-900 text-xl font-medium mb-2">
             Hashnode Roulette
           </h5>
-          <button
-            type="button"
+          <Button
             onClick={() => signIn("twitter")}
-            className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          >
-            Sign in with Twitter
-          </button>
+            text="Sign in with Twitter"
+          />
         </Card>
       )}
     </div>
