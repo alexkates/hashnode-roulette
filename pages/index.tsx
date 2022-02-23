fcaddbecc3db91c38f49eccf7fb1487fda0a5a0a
Daniel Alexander Kates <alex.kates@creditly.com>
2022-02-23 07:24:22 -0500

    started landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,19 @@ import { getSession } from "next-auth/react"
 
 export default function IndexPage() {
   return (
-    <div>
-      <h1>Hello Next.js 👋</h1>
-    </div>
+    <section className="flex flex-col mx-auto text-center pt-32">
+      <h1 className="text-4xl md:text-6xl font-extrabold tracking-tighter mb-4">
+        Make your hashnode feed{" "}
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+          wonderful
+        </span>
+      </h1>
+
+      <p className="text-xl text-gray-600 mb-8">
+        Quickly find new stories and authors to follow using simple mobile
+        gestures.
+      </p>
+    </section>
   )
 }
 
